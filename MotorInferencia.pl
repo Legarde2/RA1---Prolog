5d639b04983e9b21b110085cf3191f0256ff6e22
ViniCodemosSZ <viniciusedivaldo75@gmail.com>
2025-09-21 12:09:37 -0300

    Upload do Arquivo "MotorInferencia.pl" com alterações.
    MotorInferencia.pl com alterações e ampliações na logica.

--- MotorInferencia.pl
+++ MotorInferencia.pl
@@ -1,6 +1,21 @@
+%Predicado pra gerar ranking final de todas as trilhas
+recomenda(RankingFinal):-
+    findall(Trilha, trilha(Trilha, _), ListaTrilhas),
+
+calcular_pontuacao_total(ListaTrilhas, ListaPontuacoes),
+
+keysort(ListaPontuacoes, ListaOrdenada),
+
+reverse(ListaOrdenada, RankingFinal).
+
+%Predicado Auxiliar pra transformar a lista de nomes de trilhas em uma lista só com (Pontuacao, Trilha)
+calcular_pontuacao_total([], []).
+calcular_pontuacao_total([Trilha | RestoTrilha], [(Pontuacao, Trilha) | RestoPontuacao]) :-
+    calcular_pontuacao_trilha(Trilha, Pontuacao),
+    calcular_pontuacao_total(RestoTrilha, RestoPontuacao).
+
+%Predicado que calcula a Pontuação de uma trilha só
 calcular_pontuacao_Trilha(Trilha,Pontuacao) :-
-% Recebe a trilha, encontra todos os perfis correspondentes
-%Cria a ListaDeCriterios com pares (Caracteristica, Peso)
     findall((Caracteristica, Peso),
         perfil(Trilha, Caracteristica, Peso),
         ListaDeCriterios
@@ -8,16 +23,17 @@ calcular_pontuacao_Trilha(Trilha,Pontuacao) :-
 ),
 calcular_pontuacao(ListaDeCriterios, Pontuacao).
 
-%Parte principal para fazer o calculo da pontuação
-calcular_cabeça(Caracteristica, Peso, TotalCabeça) :-
+%Parte principal para fazer o calculo para um criterio só
+calcular_cabeca(Caracteristica, Peso, TotalCabeça) :-
     pergunta(ID, _, Caracteristica),
     resposta(ID, s),
     TotalCabeça = Peso.
 calcular_cabeça(_, _, 0).
 
+%Predicado que Soma os pontos de uma lista de criterios
+calcular_pontuacao([],0).
 calcular_pontuacao([(Caracteristica, Peso) | Resto], PontuacaoTotal) :-
 
     calcular_pontuacao(Resto, SomaResto),
     calcular_cabeça(Caracteristica, Peso, SomaAtual),
     PontuacaoTotal is SomaResto + SomaAtual.
-calcular_pontuacao([],0).
